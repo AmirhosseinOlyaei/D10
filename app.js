c7180706f5196bd698fb99c49491ae49acb29009
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2022-12-05 14:58:54 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+function bold() {
+    document.getElementById('textarea1').style.fontWeight='bold'
+}
+function italic() {
+    document.getElementById('textarea1').style.fontStyle='italic'
+}
+function tal() {
+    document.getElementById('textarea1').style.textAlign='left'
+}
+function tac() {
+    document.getElementById('textarea1').style.textAlign='center'
+}
+function tar() {
+    document.getElementById('textarea1').style.textAlign='right'
+}
+function uppc() {
+    document.getElementById('textarea1').style.textTransform='uppercase'
+}
+function lowc() {
+    document.getElementById('textarea1').style.textTransform='lowercase'
+}
+function cap() {
+    document.getElementById('textarea1').style.textTransform='Capitalize'
+}
+function clr() {
+    document.getElementById('textarea1').value=''
+}
+function clean() {
+    document.getElementById('textarea1').value='';
+}
